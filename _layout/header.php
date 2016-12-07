81d250d0500672fb9e468268bb276b617c3958e1
Luthfi Aulia Sulaiman <luthfias@yahoo.com>
2016-12-07 18:12:06 +0700

    Added carousel and minor font fixed

--- _layout/header.php
+++ _layout/header.php
@@ -20,11 +20,42 @@ $active = 'active';
     <link rel="stylesheet" href="./css/app.css" />
 </head>
 <body>
+    <div id="carousel-example-with-caption" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-with-caption" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-with-caption" data-slide-to="1"></li>
+        <li data-target="#carousel-example-with-caption" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-caption">
+          <h1>What we do</h1>
+          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        </div>
+        <div class="carousel-item active">
+          <img src="./src/img/1.jpg" width=100% alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img src="./src/img/2.jpg" width=100% alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img src="./src/img/3.jpg" width=100% alt="Third slide">
+        </div>
+      </div>
+      <a class="left carousel-control" href="#carousel-example-with-caption" role="button" data-slide="prev">
+        <span class="icon-prev" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-with-caption" role="button" data-slide="next">
+        <span class="icon-next" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
     <nav class="navbar navbar-light bg-faded">
       <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
       </button>
       <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
-        <a class="navbar-brand" href="#">Perpustakaan Mini</a>
+        <a class="navbar-brand">Perpustakaan Mini</a>
         <ul class="nav navbar-nav">
 
           <li class="nav-item  <?php if ($pages == 'home') echo $active?>">
